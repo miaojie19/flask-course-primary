e2361aafd12ae6f3d852c6efdea647e1d4a63f57
miaojie19 <miaojie19881222@gmail.com>
2019-05-22 14:07:03 +0800

    模板中使用循环

--- day2/template_demo/routes.py
+++ day2/template_demo/routes.py
@@ -4,7 +4,18 @@ from template_demo import app         # 从template_demo包中导入app实例
 
 @app.route('/')
 def index():
-    user = {'username':'猫姐'}         # 建立一个user字典
-    return render_template('index.html', html_user=user)
+    user = {'username': '猫姐'}
+    # 日志由一个列表组成，其中里面包含两个字典，里面各有author和content字段
+    posts = [
+        {
+            'author': {'username': '猫姐'},
+            'content': 'This day is Beautiful day!'
+        },
+        {
+            'author': {'username': '猫哥'},
+            'content': 'The flower is beautiful!'
+        }
+    ]
+    return render_template('index.html', title='Home', html_user=user, html_posts=posts)
 
 
