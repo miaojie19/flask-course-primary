4747b16cbe08651a098087f862a5b26ea3890dc3
miaojie19 <424277539@qq.com>
2019-06-03 17:09:23 +0800

    添加.flaskevn文件

--- day4/database_demo/__init__.py
+++ day4/database_demo/__init__.py
@@ -6,5 +6,5 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 app.config['SQLALCHEMY_DATABASE_URE'] = 'sqlite:///' + os.path.join(basedir, 'database.db')
 db = SQLAlchemy(app)
 
-from database_demo import routes
+from database_demo.routes import *
 
--- day4/database_demo/routes.py
+++ day4/database_demo/routes.py
@@ -1,3 +1,4 @@
+#!coding:utf8
 from database_demo import db
 
 
