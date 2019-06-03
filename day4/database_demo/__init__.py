ae80f73d844af76cc0d71c9689aec8ef45690800
miaojie19 <424277539@qq.com>
2019-06-03 17:59:23 +0800

    修改__init__.py文件

--- day4/database_demo/__init__.py
+++ day4/database_demo/__init__.py
@@ -3,7 +3,7 @@ from flask_sqlalchemy import SQLAlchemy
 import os
 app = Flask(__name__)
 basedir = os.path.abspath(os.path.dirname(__file__))
-app.config['SQLALCHEMY_DATABASE_URE'] = 'sqlite:///' + os.path.join(basedir, 'database.db')
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'database.db')
 db = SQLAlchemy(app)
 
 from database_demo.routes import *
