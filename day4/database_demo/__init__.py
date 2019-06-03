69969de9a35fc4832359b1fb3ca54cdb5fd0c504
miaojie19 <424277539@qq.com>
2019-06-04 00:01:53 +0800

    添加数据库迁移代码

--- day4/database_demo/__init__.py
+++ day4/database_demo/__init__.py
@@ -1,10 +1,14 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+
 import os
+
 app = Flask(__name__)
 basedir = os.path.abspath(os.path.dirname(__file__))
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'database.db')
 db = SQLAlchemy(app)
+migrate = Migrate(app, db)
 
 from database_demo.routes import *
 
--- day4/database_demo/routes.py
+++ day4/database_demo/routes.py
@@ -1,11 +1,12 @@
 #!coding:utf8
-from database_demo import db
+from database_demo import db, app
 
 
 class User(db.Model):
     __table__name = 'user'
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(20), unique=True, nullable=False)
+    email = db.Column(db.String(120), unique=False, nullable=True)
     #添加反向引用关系
     posts = db.relationship('Post', backref='author', lazy=True)
 
@@ -17,8 +18,15 @@ class Post(db.Model):
     __table__name = 'post'
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(100), unique=True, nullable=False)
+    content = db.Column(db.Text, unique=False, nullable=True)
     # 添加user_id外键，它建立表user和表post的联系
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
 
     def __repr__(self):
         return f"Post('{self.title}')"
+
+
+@app.shell_context_processor
+def generate_shell_context():
+    return {'db': db, 'User': User, 'Post': Post}
+
